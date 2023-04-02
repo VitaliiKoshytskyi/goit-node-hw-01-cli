b4f2f24a11a366974ea63e34c8639f80a97e4667
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-04-02 18:16:43 +0300

    updated markup

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hey boy hey girl")
+
+const contactsService = require("./contacts.js")
+
+// const argv = require("yargs").argv;
+
+
+const invokeAction = async ({ action, id, name, email, phone })=> {
+  switch (action) {
+    case "list":
+          const allContacts = await contactsService.listContacts();
+          console.log(allContacts)
+
+      break;
+
+    case "get":
+      // ... id
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction(argv);
+invokeAction({ action: "list" })
